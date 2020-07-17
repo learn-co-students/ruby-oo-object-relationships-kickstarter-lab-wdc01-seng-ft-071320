0640b042a31ccb385ea4208ef7725dbb2ea17591
Kristian Portillo <kristianportillo@Kristians-MacBook-Air.local>
2020-07-17 14:10:48 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+class Backer
+    attr_reader :name
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        matching_projects = ProjectBacker.all.select do |pr|
+            pr.backer == self
+        end
+        matching_projects.collect do |match| 
+        match.project
+        end
+    end
+end
