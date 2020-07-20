cfaae60460b9e42010ef6ee6267e386cf37be7f6
solomonfy <fantahunsolomon@gmail.com>
2020-07-19 23:45:51 -0400

    Done

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,13 +10,14 @@ class Backer
     end
 
     def back_project(project)
-    ProjectBacker.new(project, self)
+        ProjectBacker.new(project, self)
     end
 
     def backed_projects
         matching_record = ProjectBacker.all.select {|single_pb| single_pb.backer == self}
-            matching_record.map {|match| match.project}
-            
+        matching_record.map(&:project)
+        # OR
+        # ProjectBacker.all.select {|single_pb| single_pb.backer == self}.map(&:project)
     end
 
 
